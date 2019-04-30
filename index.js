d81bdaeb05c3611e08162a43c4c8478e6da4a0af
PAN Zhiqi <zhiqipan@yahoo.com>
2019-04-30 18:02:14 +0800

    Support notification

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+const path = require('path');
 const alfy = require('alfy');
 const runApplescript = require('run-applescript');
+const notifier = require('node-notifier');
 
 const run = async () => {
   const input = alfy.input;
   if (typeof input !== 'string') return;
 
   const args = input.split(/\s+/);
-  const [delay, message] = args;
-  if (delay && message) {
+  const [delay, message = ''] = args;
+  if (delay && (parseInt(delay) > 0 || parseInt(delay.substr(0, delay.length - 1)) > 0)) {
     let seconds = delay;
     const unit = delay.substr(-1);
     if (isNaN(parseInt(unit))) {
@@ -26,18 +28,32 @@ const run = async () => {
       seconds = value;
     }
   }
-    
-  setTimeout(async () => {
-    await runApplescript﻿(`
-      set alertReply to display alert "Time's up (${delay})" message "${message}" buttons ["OK"] default button 1 giving up after 5
-      if button returned of alertReply is equal to "OK" then
-        # tell application "Chrome"
-        #   activate
-          
-        # end tell
-      end if
-    `);
-  }, seconds * 1000);
+
+  notifier.notify({
+    title: 'Timer is set up',
+    message: delay,
+    icon: path.join(__dirname, 'icon.png'),
+    timeout: 2,
+  });
+  
+  await runApplescript﻿(`
+    delay ${seconds}
+    set alertReply to display alert "Time's up (${delay})" message "${message}" buttons ["OK"] default button 1 giving up after 5
+    if button returned of alertReply is equal to "OK" then
+      # tell application "Chrome"
+      #   activate
+        
+      # end tell
+    end if
+  `);
+
+  } else {
+    notifier.notify({
+      title: 'Oops...',
+      message: 'Invalid parameters',
+      icon: path.join(__dirname, 'icon.png'),
+      timeout: 2,
+    });
   }
 }
 
